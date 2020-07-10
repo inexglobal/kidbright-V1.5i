dae8d4a56900eee3ae01ba3fc5c235aa467c50c5
inexglobal <chaiwat@inex.co.th>
2020-07-10 21:36:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "kidbright-V1.5i",
   platform: "arduino-esp32",
-  title: "Kidbright32 1.5i (Arduino API)",
+  title: "Kidbright32 1.5i(Arduino API)",
   description: "บอร์ด KidBright32 V1.5i by INEX (บอร์ด KidBright32 รุ่นที่ผลิตโดย INEX)เขียนบนแพลตฟอร์ม Arduino-ESP32 ทำให้สามารถใช้ความสามารถใหม่ ๆ และปลั๊กอินของ Arduino ได้",
   author: "Innovative Experiment",
   website: "https://inex.co.th/shop/kidbright-v1-5.html",
